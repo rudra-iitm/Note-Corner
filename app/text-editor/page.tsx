c3446f8e1005795e53c35ad98e4eeaec6d542564
sushant-wayal <sushantssr12345@gmail.com>
2024-04-14 23:34:31 +0530

    code ditor component

--- app/text-editor/page.tsx
+++ app/text-editor/page.tsx
@@ -1,32 +1,11 @@
 "use client"
 
-import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'quill/dist/quill.snow.css'; // Or your desired theme CSS
+import CodeEditor from "@/components/CodeEditor"
 
 const page = () => {
-  const [editorContent, setEditorContent] = useState('');
-  const handleEditorChange = (value: string) => {
-    setEditorContent(value);
-  };
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ['bold', 'italic', 'underline', 'strikethrough'],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      ['link', 'image']
-    ],
-  };
   return (
-    <div>
-      <ReactQuill
-        theme="snow"
-        value={editorContent}
-        onChange={handleEditorChange}
-        modules={modules}
-      />
-    </div>
-  );
+    <CodeEditor />
+  )
 }
 
 export default page
