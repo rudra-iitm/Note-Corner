e0b1ec4ff2712d651ae9115df9d9750a40e2c06c
Shivam Jaiswal <shivam181106@gmail.com>
2024-04-14 23:52:33 +0530

    Rich Text editor

---
+++ components/RichTextEditor.tsx
@@ -0,0 +1,37 @@
+import ReactQuill from 'react-quill';
+import 'quill/dist/quill.snow.css'; // Or your desired theme CSS
+import { useState } from 'react';
+
+const RichTextEditor = () => {
+    const [editorContent, setEditorContent] = useState('');
+    const handleEditorChange = (value: string) => {
+        setEditorContent(value);
+      };
+      const modules = {
+        toolbar: [
+            [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+            [{size: []}],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{'list': 'ordered'}, {'list': 'bullet'},
+             {'indent': '-1'}, {'indent': '+1'}],
+            ['link', 'image', 'video', 'code-block', 'formula'],
+            ['clean'],
+            ['code-block'],
+        ],
+        clipboard: {
+          matchVisual: false,
+        },
+      };
+   return (
+    <ReactQuill
+        theme="snow"
+        value={editorContent}
+        onChange={handleEditorChange}
+        modules={modules}
+        formats={['code-block']}
+        className='w-full flex-grow px-3 py-5 text-white mb-0'
+      />
+   ) 
+}
+
+export default RichTextEditor;
