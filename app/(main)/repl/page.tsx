bd53788e2aa1d65c8b99651eb60c6f24c373cac2
Rudra-IITM <singhrudra5556@gmail.com>
2024-04-15 02:09:26 +0530

    repl

---
+++ app/(main)/repl/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+    return (
+        // <iframe src="https://replit.com/@rudra993/RemorsefulPrizeRecords?embed=true" width="600" height="400"></iframe>  
+        <iframe 
+    width="100%"
+    height="350"    
+    src="data:text/html;charset=utf-8,
+    <head><base target='_blank' /></head>
+    <body><script src='https://gist.github.com/Albert-W/e37d1c4fa30c430c37d7b1b1fe9b60d8.js'></script>
+    </body>"></iframe>   
+    );
+}
+
