0dec2f465da45109cf3f4aa4ae96a5473a4d9770
Rudra-IITM <singhrudra5556@gmail.com>
2024-04-15 13:30:17 +0530

    ui correction in todo

--- components/InputTodo.tsx
+++ components/InputTodo.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState } from "react";
 import { useTodo } from "../Context/Todo";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { CiCirclePlus } from "react-icons/ci";
 
 const InputTodo = () => {
   const { addTodo } = useTodo();
@@ -13,8 +14,8 @@ const InputTodo = () => {
   }
   return (
     <form onSubmit={addNewTodo} className="w-full h-8 rounded-3xl mb-8 flex gap-1">
-      <Input className="w-11/12 mr-1 h-full rounded pl-3 outline-2 outline-zinc-900 dark:outline-zinc-400 outline opacity-50" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} placeholder="Write Todo..."/>
-			<Button className="h-full hover:bg-blue-800 text-lg" type="submit">+</Button>
+      <Input className="flex-1 rounded-lg p-4" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} placeholder="Write Todo..."/>
+			<Button className="rounded-lg w-16 h-10 ml-4" type="submit"><CiCirclePlus size={20}/></Button>
 		</form>
     )
 }
--- components/Todo.tsx
+++ components/Todo.tsx
@@ -4,6 +4,10 @@ import { Checkbox } from "./ui/checkbox";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Pencil, Save, Trash2 } from "lucide-react";
+import { HiOutlinePencilAlt } from "react-icons/hi";
+import { IoIosSave } from "react-icons/io";
+import { MdOutlineDelete } from "react-icons/md";
+import { Card } from "./ui/card";
 
 interface TodoProps {
   todo: {
@@ -37,16 +41,18 @@ const Todo : React.FC<TodoProps> = ({ todo }) => {
   }
   return (
     <div className="w-full h-12">
-      <form onSubmit={handleSubmit} className={`h-full w-full flex gap-3 justify-center items-center px-4 py-2 rounded-xl ${todo.complete ? "bg-green-600 " : "bg-zinc-700 dark:bg-zinc-200"}`}>
-        <Checkbox checked={todo.complete} onClick={() => toggleComplete(todo.id)} className="bg-zinc-300 "/>
-        <Input className={`w-10/12 p-2 border-0 outline-2 outline-zinc-900 ${todo.complete ? "line-through" : ""}`} type="text" readOnly={!editable} value={thisTodo} onChange={(e) => setThisTodo(e.target.value)} />
-        <Button variant={editable ? "default" : "outline"} className="w-1/12 h-full" type="submit">
-          {editable ? <Save size={20}/> : <Pencil size={20}/> }
-        </Button>
-        <Button variant="destructive" className="w-1/12 h-full" onClick={deleteThisTodo}>
-          <Trash2 size={20}/>
-        </Button>
-      </form>
+      <Card>
+        <form onSubmit={handleSubmit} className={`h-full w-full flex gap-3 justify-center items-center px-4 py-2 rounded-xl`}>
+          <Checkbox checked={todo.complete} onClick={() => toggleComplete(todo.id)} className=""/>
+          <Input className={`w-10/12 p-2 border-0 outline-2  ${todo.complete ? "line-through" : ""}`} type="text" readOnly={!editable} value={thisTodo} onChange={(e) => setThisTodo(e.target.value)} />
+          <Button variant={editable ? "default" : "outline"} className="w-1/12 h-full" type="submit">
+            {editable ? <IoIosSave size={20}/> : <HiOutlinePencilAlt size={20}/> }
+          </Button>
+          <Button className="w-1/12 h-full bg-red-600 rounded-lg" onClick={deleteThisTodo}>
+            <MdOutlineDelete size={20}/>
+          </Button>
+        </form>
+      </Card>
     </div>
   )
 }
