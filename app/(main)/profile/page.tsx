c86ea3f7925e401b4bc3c901292fa2de850019f3
Rudra-IITM <singhrudra5556@gmail.com>
2024-04-16 02:41:34 +0530

    bhut chiz hua h

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,10 +1,12 @@
 import Profile from '@/components/Profile'
+import { SidebarDrawer } from '@/components/SidebarDrawer'
 import Dashboard from '@/components/ui/Dashboard'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
+      <SidebarDrawer urll={'profile'}/>
       <Dashboard/>
     </div>
   )
