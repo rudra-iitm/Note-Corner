27442c04941ff762a891b9d2d75c32a7399fc524
RudraIIT <b22125@students.iitmandi.ac.in>
2024-04-15 21:48:15 +0530

    Chatbot Integration

--- components/ChatBot.tsx
+++ components/ChatBot.tsx
@@ -1,9 +1,9 @@
 import { Bubble } from "@typebot.io/nextjs";
 
 export const ChatBot = () => {
-return (<Bubble
-      typebot="faq-osyrptm"
-      theme={{ button: { backgroundColor: "#0042DA" } }}
-    />
-);
+  return (<Bubble
+    typebot="faq-osyrptm"
+    theme={{ button: { backgroundColor: "#0042DA" } }}
+  />
+  );
 };
